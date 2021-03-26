6a58a46a1a185e7919070ec24f23e6ed352332d0
Aljosa Mohorovic <mohorovic@missionbio.com>
2021-03-26 16:14:03 +0100

    Fix plotly #1

--- build/hooks/hook-plotly.py
+++ build/hooks/hook-plotly.py
@@ -1,8 +1,9 @@
-from PyInstaller.utils.hooks import collect_all, collect_data_files
+from PyInstaller.utils.hooks import collect_all, collect_data_files, collect_submodules
 
 datas = collect_data_files(
     'plotly',
     include_py_files=True,
     includes=['**/*.py', 'package_data/**/*.*']
 )
-# datas, binaries, hiddenimports = collect_all('plotly')
+_, binaries, hiddenimports = collect_all('plotly')
+hiddenimports += collect_submodules('plotly.graph_objs')
