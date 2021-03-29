390950e8d685e00409af6f7aee0df192914c94a6
Aljosa Mohorovic <mohorovic@missionbio.com>
2021-03-29 12:43:44 +0200

    Fix plotly #3

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@ pyinstaller --additional-hooks-dir=hooks \
 			--hidden-import missionbio.mosaic \
 			--hidden-import missionbio.h5 \
 			--hidden-import plotly \
-			--clean run.py
+			--clean -w run.py
 
-cp ../*.py ./dist/run/
-cp -r ../tasks ./dist/run/
+cp ../*.py ./dist/run.app/Contents/Resources/
+cp -r ../tasks ./dist/run.app/Contents/Resources/
