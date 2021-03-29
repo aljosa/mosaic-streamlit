8c7e21d671bab1047e4f01ccd4391bf5a237aad6
Aljosa Mohorovic <mohorovic@missionbio.com>
2021-03-29 15:01:52 +0200

    Fix plotly #7

--- build/run.py
+++ build/run.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     # streamlit can take a while to import
     from streamlit import cli as stcli
 
-    sys.argv = ["streamlit", "run", f"{launchdir}/app.py", "--server.port=10000", "--server.headless=true", "--global.developmentMode=false"]
+    sys.argv = ["streamlit", "run", f"{launchdir}/app.py", "--server.port=10000", "--server.headless=true", "--server.fileWatcherType=none", "--global.developmentMode=false"]
     sys.exit(stcli.main())
